9ee182864f216221ef79a41f32a0711dfd08ccb5
andresattler <andresattler.webdev@gmail.com>
2017-09-30 12:10:30 +1300

    code clean up

--- src/index.js
+++ src/index.js
@@ -1,15 +1,60 @@
+/* global fetch */
 import { install } from 'offline-plugin/runtime'
 
 import './style.styl'
 
 import createField from './create-field'
 import next from './next'
+import transform from './transform'
 import webGLRenderer from './web-gl-renderer'
 
+// install service-worker
 if (process.env.NODE_ENV === 'production') {
   install()
 }
 
+// global vars
+const state = {
+  play: false,
+  speed: 500,
+  pattern: Array.from(new Array(54000), () => false),
+  width: 300,
+  height: 180,
+  nrOfCells: 54000
+}
+
+const fieldEl = document.getElementById('field')
+const playPauseIcon = document.getElementById('play-pause-icon')
+const fieldModel = () => createField(state.width, state.height)
+
+function updateFieldDimensions () {
+  fieldEl.width = window.innerWidth
+  fieldEl.height = window.innerHeight * 0.9
+  state.size = fieldEl.width / state.height
+}
+
+function newFieldDimensions () {
+  state.size = Math.sqrt((window.innerWidth * (window.innerHeight * 0.9)) / state.nrOfCells)
+  state.width = Math.floor(window.innerWidth / state.size)
+  state.height = state.nrOfCells / state.width
+}
+
+function render (nextPattern) {
+  state.pattern = nextPattern.slice()
+  webGLRenderer(transform(state))
+}
+
+function paint (cX, cY, draw) {
+  const nextPattern = state.pattern.slice()
+  const size = fieldEl.width / state.width
+  const row = Math.floor(cY / size)
+  const x = Math.floor(cX / size - 1)
+  const index = x + (row * state.width) + 1
+  nextPattern[index] = draw ? true : !nextPattern[index]
+  render(nextPattern)
+}
+
+// initialize app
 fetch('./public/patterns/index.json')
 .then(data => data.json())
 .then((json) => {
@@ -23,64 +68,17 @@ fetch('./public/patterns/index.json')
   })
 })
 
-const state = {
-  play: false,
-  speed: 500,
-  field: Array.from(new Array(54000), () => false),
-  width: 300,
-  height: 180,
-  nrOfCells: 54000
-}
-
-const fieldEl = document.getElementById('field')
-const fieldModel = () => createField(state.width, state.height)
-
-function updateFieldDimensions () {
-  fieldEl.width = window.innerWidth
-  fieldEl.height = window.innerHeight * 0.9
-  state.size = fieldEl.width / state.height
-}
-
 updateFieldDimensions()
 newFieldDimensions()
+render(state.pattern)
 
-function transform (pattern) {
-  const v = []
-  state.field = pattern.slice()
-  let row = 0
-  let x = 0
-  let y = 0
-  const size = state.size
-  for (let i = 0; i < state.nrOfCells; i += 1) {
-    x += size
-    if (state.field[i]) {
-      v.push(
-        x, y, // left up
-        x + size, y, // right up
-        x, y + size, // left down
-        // 2nd triangle
-        x, y + size, // left down
-        x + size, y + size, // right down
-        x + size, y  // right up
-      )
-    }
-    if (i === state.width * (row + 1) - 1) {
-      row++
-      y += size
-      x = 0
-    }
-  }
-  return v
+function pause () {
+  state.play = false
+  playPauseIcon.classList.toggle('fa-play')
+  playPauseIcon.classList.toggle('fa-pause')
 }
 
-const randomPattern = length => [...Array(length)].map(cell => Math.random() > 0.75)
-
-function newFieldDimensions () {
-  state.size = Math.sqrt((window.innerWidth * (window.innerHeight * 0.9)) / state.nrOfCells)
-  state.width = Math.floor(window.innerWidth / state.size)
-  state.height = state.nrOfCells / state.width
-}
-
+// event handlers
 function handleRezise () {
   let resizeTimeout
   if (!resizeTimeout) {
@@ -89,68 +87,6 @@ function handleRezise () {
       updateFieldDimensions()
     })
   }
-}
-
-function handleRandom () {
-  newFieldDimensions()
-  webGLRenderer(transform(randomPattern(state.nrOfCells)))
-}
-function handleNext () {
-  webGLRenderer(transform(next(fieldModel(), state.field)))
-}
-function handlePlay (e) {
-  state.play = !state.play
-  e.target.children[0].classList.toggle('fa-play')
-  e.target.children[0].classList.toggle('fa-pause')
-  function sleep (ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-  }
-  const play = () => {
-    sleep(state.speed).then(() => {
-      webGLRenderer(transform(next(fieldModel(), state.field)))
-      if (state.play) {
-        play()
-      }
-    })
-  }
-  play()
-}
-function pause () {
-  state.play = false
-  document.getElementById('play-pause').innerHTML = 'Play'
-}
-function handleClear () {
-  newFieldDimensions()
-  state.play && pause()
-  state.field = [...Array(state.nrOfCells)].map(() => false)
-  webGLRenderer([])
-}
-
-function paint (cX, cY, draw) {
-  const size = fieldEl.width / state.width
-  const row = Math.floor(cY / size)
-  const x = Math.floor(cX / size - 1)
-  const index = x + (row * state.width) + 1
-  state.field[index] = draw ? true : !state.field[index]
-  // TO DO!!
-  webGLRenderer(transform(state.field))
-}
-
-function handlePaint (e) {
-  paint(e.clientX, e.clientY)
-}
-
-function handleDraw (e) {
-  function stop () {
-    e.target.removeEventListener('mousemove', draw)
-  }
-  function draw (e) {
-    paint(e.clientX, e.clientY, true)
-  }
-  e.target.addEventListener('touchmove', (e) => draw(e.changedTouches[0]))
-  e.target.addEventListener('mousemove', draw)
-  e.target.addEventListener('touchend', (e) => stop(e.changedTouches[0]))
-  e.target.addEventListener('mouseup', stop)
 }
 
 function SpeedDown (e) {
@@ -167,7 +103,42 @@ function SpeedUp () {
   }
 }
 
+function handleRandom () {
+  newFieldDimensions()
+  const randomPattern = length => Array.from(new Array(length), () => cell => Math.random() > 0.75)
+  render(randomPattern(state.nrOfCells))
+}
+
+function handlePlay (e) {
+  state.play = !state.play
+  playPauseIcon.classList.toggle('fa-play')
+  playPauseIcon.classList.toggle('fa-pause')
+  function sleep (ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+  const play = () => {
+    sleep(state.speed).then(() => {
+      render(next(fieldModel(), state.pattern))
+      if (state.play) {
+        play()
+      }
+    })
+  }
+  play()
+}
+
+function handleNext () {
+  render(next(fieldModel(), state.pattern))
+}
+
+function handleClear () {
+  newFieldDimensions()
+  state.play && pause()
+  render(Array.from(new Array(state.nrOfCells), () => false))
+}
+
 function handleAdd (e) {
+  newFieldDimensions()
   const filename = e.target.dataset.filename
   if (filename) {
     fetch(`./public/patterns/${filename}.json`)
@@ -188,7 +159,7 @@ function handleAdd (e) {
         ...middle,
         ...Array.from(new Array(startY * state.width), () => false)
       ]
-      webGLRenderer(transform(nextState))
+      render(nextState)
       toggleAddModal()
     })
   }
@@ -198,19 +169,38 @@ function toggleAddModal () {
   document.getElementById('modal-add').classList.toggle('visible')
 }
 
-window.addEventListener('resize', handleRezise)
-document.getElementById('name-list').addEventListener('click', handleAdd)
-document.getElementById('add-modal-btn').addEventListener('click', toggleAddModal)
-document.getElementById('modal-add').addEventListener('click', (e) => {
-  e.target === e.currentTarget && toggleAddModal()
+function handlePaint (e) {
+  paint(e.clientX, e.clientY)
 }
-)
-document.getElementById('random').addEventListener('click', handleRandom)
-document.getElementById('next').addEventListener('click', handleNext)
-document.getElementById('play-pause').addEventListener('click', handlePlay)
+
+function handleDraw (e) {
+  function stop () {
+    e.target.removeEventListener('mousemove', draw)
+  }
+  function draw (e) {
+    paint(e.clientX, e.clientY, true)
+  }
+  e.target.addEventListener('touchmove', (e) => draw(e.changedTouches[0]))
+  e.target.addEventListener('mousemove', draw)
+  e.target.addEventListener('touchend', (e) => stop(e.changedTouches[0]))
+  e.target.addEventListener('mouseup', stop)
+}
+
+window.addEventListener('resize', handleRezise)
+
 document.getElementById('speed-up').addEventListener('click', SpeedUp)
 document.getElementById('speed-down').addEventListener('click', SpeedDown)
+document.getElementById('random').addEventListener('click', handleRandom)
+document.getElementById('play-pause').addEventListener('click', handlePlay)
+document.getElementById('next').addEventListener('click', handleNext)
 document.getElementById('clear').addEventListener('click', handleClear)
+document.getElementById('add-modal-btn').addEventListener('click', toggleAddModal)
+
+document.getElementById('name-list').addEventListener('click', handleAdd)
+document.getElementById('modal-add').addEventListener('click', (e) => {
+  e.target === e.currentTarget && toggleAddModal()
+})
+
 document.getElementById('field').addEventListener('click', handlePaint)
 document.getElementById('field').addEventListener('touchstart', (e) => { handleDraw(e.changedTouches[0]) })
 document.getElementById('field').addEventListener('mousedown', handleDraw)
---
+++ src/transform.js
@@ -0,0 +1,32 @@
+// transforms state to vertices
+function transform (state) {
+  const v = []
+  let row = 0
+  let x = 0
+  let y = 0
+  const size = state.size
+  for (let i = 0; i < state.nrOfCells; i += 1) {
+    x += size
+    if (state.pattern[i]) {
+      v.push(
+        // 1st polygon
+        x, y, // left up
+        x + size, y, // right up
+        x, y + size, // left down
+        // 2nd polygon
+        x, y + size, // left down
+        x + size, y + size, // right down
+        x + size, y  // right up
+      )
+    }
+    if (i === state.width * (row + 1) - 1) {
+      // switching to the next row
+      row++
+      y += size
+      x = 0
+    }
+  }
+  return v
+}
+
+export default transform
